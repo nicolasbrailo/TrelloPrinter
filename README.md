dd319362747d047b32df89caca4a273dab79fadc
Nico <nicolasbrailo@gmail.com>
2015-01-07 20:04:05 +0100

    V1

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
 TrelloPrinter
 =============
 
-Print your trello boards to html pages or to neat pdf bundles including attachments
+Trello board printer: generate a printable version of a Trello board.
+
+Usage: Open a Trello board, go to the menu of the board and click the "Share, Print and Export" option. Click the "Export to JSON" option and download the resulting json file. Call this program using the downloaded file as input. For example:
+
+    python trello_printer.py <trello_board.json -o MyBoard
+
+trello_printer.py will then:
+ 1. Create a printable version of the board, including card descriptions.
+ 2. Download all the (non-archived) card attachments which are stored in Amazon.
+ 3. Bundle the printable version of the board with the downloaded attachments into MyBoard.pdf.
+
+You can change the board print template by editing trello_printer.py. You can goto https://github.com/nicolasbrailo/TrelloPrinter and request a user-friendlier template edition. Actually, you can go over there and request any feature you'd like.
 
 Dependencies:
 =============
-pip install pystache pdfkit
+Python, wkhtmltopdf, ghostscript + pystache and pdfkit
+In Ubuntu:
+
+    sudo apt-get install wkhtmltopdf ghostscript python python-pip 
+    pip install pystache pdfkit
 
 
--- trello_printer.py
+++ trello_printer.py
@@ -1,33 +1,39 @@
-import pystache
-#sudo apt-get install wkhtmltopdf
-import pdfkit
-import urllib2
-import json
-import sys
-import cgi
-import codecs
-import time
-
-template = """
+# Template used to print a Trello board. Uses pystache.
+board_template = """
 <html>
 <head>
-<meta content="text/html;charset=
-utf-8" http-equiv="Content-Type">
+<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
 <meta content="utf-8" http-equiv="encoding">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
 <style>
-body
-{
-  font-size: 10px;
-} .card {
+.card {
   margin: 5px;
   border:1px solid black;
   width: 48%;
   display: inline-block;
   vertical-align: top;
 } .list {
-  width: 99%;
+  clear: both;
+  margin-top: 10px;
+  padding-top: 10px;
+  border-top:3px dotted black;
+}
+
+@media print
+{   
+    .no-print, .no-print *
+    {
+        display: none !important;
+    }
+
+    div{
+        page-break-inside: avoid;
+    }
+
+    a[href]:after {
+      content: none !important;
+    }
 }
 </style>
 </head>
@@ -38,7 +44,6 @@ by {{#members}}{{{fullName}}}, {{/members}}Last activity: {{dateLastActivity}} /
 <a href='{{url}}'>{{url}}</a>
 
 {{#activeLists}}
-  <hr class="row"/>
   <div class="list">
   <h3>{{{name}}}</h3>
 
@@ -49,15 +54,17 @@ by {{#members}}{{{fullName}}}, {{/members}}Last activity: {{dateLastActivity}} /
       {{{desc}}}
       </p>
 
-      Updated: {{dateLastActivity}}
-      {{#attachments}} | <a href="{{url}}">{{name}}</a>{{/attachments}}
-
-      <!--
-      {#attachments}
-        <embed type='application/pdf' src="{url}" width="100%" height="100%">
-      {/attachments}
-      -->
-
+      {{#checklists}}
+        <ul>
+        {{#checkItems}}
+          <li>{{{name}}}</li>
+        {{/checkItems}}
+        </ul>
+      {{/checklists}}
+ 
+      <div class="no-print">
+        {{#attachments}} | <a href="{{url}}">{{name}}</a>{{/attachments}}
+      </div>
     </div>
   {{/activeCards}}
   </div>
@@ -68,19 +75,21 @@ by {{#members}}{{{fullName}}}, {{/members}}Last activity: {{dateLastActivity}} /
 <html>
 """
 
-def pretty_print(jstuff):
-  print json.dumps(jstuff, sort_keys=True, indent=4, separators=(',', ': '))
+
+import pystache
+import cgi
+import codecs
 
 class Trello_Board(object):
   def __init__(self, json_object):
-    self.board = self._transmogrify_trello_board(json_object)
+    self.board_json = self._transmogrify_trello_board(json_object)
 
-  def html_render(self):
-    return pystache.render(template, board.board).encode('utf-8')
+  def html_render(self, template):
+    return pystache.render(template, self.board_json).encode('utf-8')
 
   def get_pdf_attachments(self):
     attachments = []
-    for lst in self.board['activeLists']:
+    for lst in self.board_json['activeLists']:
       for card in lst['activeCards']:
         for attach in card['attachments']:
           if attach['url'].endswith(".pdf"):
@@ -101,6 +110,8 @@ class Trello_Board(object):
     return obj
 
   def _transmogrify_trello_board(self, board):
+    """ Transform a plain Trello format (list of stuff) to a hierarchical model
+    more suitable for pystache """
     board = self._prettyHtml(board, ['name'])
     board = self._formatDates(board, ['dateLastActivity'])
 
@@ -119,6 +130,14 @@ class Trello_Board(object):
                              if card['idList'] == lst['id'] \
                                 and not card['closed'] ]
 
+      # Get all the active checklists
+      for card in lst['activeCards']:
+        card['checklists'] = [checklist for checklist in board['checklists']
+                                if checklist["idCard"]==card["id"] ]
+        for cl in card['checklists']:
+          for item in cl['checkItems']:
+            item = self._prettyHtml(item, ['name'])
+
       lst = self._prettyHtml(lst, ['name'])
       board['activeLists'].append(lst)
 
@@ -126,28 +145,131 @@ class Trello_Board(object):
 
 
 
-trello_json = ""
-for ln in codecs.getreader('utf-8')(sys.stdin).readlines():
-  trello_json += ln
-
-board = Trello_Board(json.loads(trello_json))
-#pretty_print(board.board)
-#print board.html_render()
-
-def get_fname(url):
-  return url[url.rfind('/')+1:]
-
-pdfkit.from_string(board.html_render(), 'trello_board.pdf')
-downloaded = []
-for url in board.get_pdf_attachments():
-  fname = get_fname(url)
-  open(fname, "w+").write( urllib2.urlopen(url).read() )
-  downloaded.append(fname)
-
-to_join = 'trello_board.pdf ' + ' '.join(downloaded)
-pdf_join = 'gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=out.pdf ' + to_join
-
+import json
+import cgi
+import codecs
+import sys
+import tempfile
+import pdfkit
 import os
-os.system(pdf_join)
+import urllib2
 
+
+def pretty_print(jstuff):
+  return json.dumps(jstuff, sort_keys=True, indent=4, separators=(',', ': '))
+
+
+def read_json_board(fp):
+  trello_json = ""
+  for ln in codecs.getreader('utf-8')(fp).readlines():
+    trello_json += ln
+
+  return Trello_Board(json.loads(trello_json))
+
+def create_pdf_bundle(args, board):
+  # List of all pdf files to join
+  pdfs_to_join = []
+
+  # Print temp pdf board from html, to be used by pdf joiner
+  tmp_board_pdf = tempfile.NamedTemporaryFile(delete=False)
+  if not args.quiet: print >> sys.stderr, "Generating temporary board pdf in {}...".format(tmp_board_pdf.name)
+  pdfkit.from_string(board.html_render(board_template), tmp_board_pdf.name)
+  pdfs_to_join.append(tmp_board_pdf)
+
+  # Fetch the board's attachments
+  if args.bundle_attachments:
+    for url in board.get_pdf_attachments():
+      f = tempfile.NamedTemporaryFile(delete=False)
+      if not args.quiet: print >> sys.stderr, "Downloading attachment {} into {}...".format(url, f.name)
+      f.write( urllib2.urlopen(url).read() )
+      pdfs_to_join.append(f)
+
+  # exec
+  out_fname = args.out_fname
+  if out_fname is None:
+    out_fname = sys.argv[0] + '_bundle.pdf'
+  else:
+    out_fname += '.pdf'
+
+  # Save the fnames and close them, if we hold these files open ghostscript goes haywire
+  join_fnames = ' '.join([f.name for f in pdfs_to_join])
+  for f in pdfs_to_join:
+    f.close()
+
+  if not args.quiet: print >> sys.stderr, "Generating bundle {}:".format(out_fname)
+  pdf_join_cmd = 'gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile={} {}'\
+                    .format(out_fname, join_fnames)
+  if not args.quiet: print >> sys.stderr, "Running {}".format(pdf_join_cmd)
+  os.system(pdf_join_cmd)
+
+  # Clean up temp files
+  for fn in pdfs_to_join: os.remove(fn.name)
+
+
+def main(args):
+  board = read_json_board(sys.stdin)
+  if not args.quiet: print >> sys.stderr, "Valid board found!"
+
+  if args.html_output:
+    f = sys.stdout
+    if args.out_fname is not None:
+      f = open(args.out_fname + '.html', 'w+')
+    print >> f, board.html_render(board_template)
+
+  if args.print_debug_json:
+    f = sys.stdout
+    if args.out_fname is not None:
+      f = open(args.out_fname + '.json', 'w+')
+    print >> f, pretty_print(board.board_json)
+
+  if args.create_pdf_bundle:
+    create_pdf_bundle(args, board)
+
+
+import argparse
+import sys
+
+app_desc = """
+Trello board printer: generate a printable version of a Trello board.
+
+Usage: Open a Trello board, go to the menu of the board and click the "Share, Print and Export" \
+option. Click the "Export to JSON" option and download the resulting json file. Call this program \
+using the downloaded file as input. For example:
+
+    python {0} <trello_board.json -o MyBoard
+
+{0} will then:
+ 1. Create a printable version of the board, including card descriptions.
+ 2. Download all the (non-archived) card attachments which are stored in Amazon.
+ 3. Bundle the printable version of the board with the downloaded attachments into MyBoard.pdf.
+
+You can change the board print template by editing {0}. You can goto \
+https://github.com/nicolasbrailo/TrelloPrinter and request a user-friendlier template edition. \
+Actually, you can go over there and request any feature you'd like.
+
+"""
+parser = argparse.ArgumentParser(description=app_desc.format(sys.argv[0]),
+                                 formatter_class=argparse.RawDescriptionHelpFormatter)
+parser.add_argument("-o", "--output", dest="out_fname",
+                    default=None, metavar="FILE_NAME",
+                    help="Use FILE_NAME for output (eg: Specify '-o MyBoard --html \
+                          --debug' to generate MyBoard.json and MyBoard.html).")
+parser.add_argument("--no-attachments", action="store_false",
+                    dest="bundle_attachments", default=True,
+                    help="Don't download and bundle pdf attachments in final printable document.")
+parser.add_argument("--html", action="store_true",
+                    dest="html_output", default=False,
+                    help="Output only an HTML version of the board.")
+parser.add_argument("--no-bundle", action="store_false",
+                    dest="create_pdf_bundle", default=True,
+                    help="Don't create a pdf bundle of the specified board.")
+parser.add_argument("--debug", action="store_true",
+                    dest="print_debug_json", default=False,
+                    help="Output a prettyfied version of the board's json.")
+parser.add_argument("-q", "--quiet", action="store_true",
+                    dest="quiet", default=False,
+                    help="Run in quiet mode.")
+
+if __name__ == '__main__':
+  main(parser.parse_args())
 
